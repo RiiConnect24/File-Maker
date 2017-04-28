96d83d7c78e89006f84cdf52c929516c9a511102
larsenv <larsen@weborican.com>
2017-04-28 16:20:37 -0500

    Fixed Norway and New Zealand lists

--- Channels/Forecast_Channel/listsorter.py
+++ Channels/Forecast_Channel/listsorter.py
@@ -5,7 +5,7 @@ import collections
 import forecastlists
 import forecastregions
 
-weathercities = [] # Edit this to include the dictionaries to use.
+weathercities = [forecastlists.weathercities096] # Edit this to include the dictionaries to use.
 
 for weather in weathercities:
 	weathercity = collections.OrderedDict()
