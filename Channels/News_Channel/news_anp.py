d69114aea463791e66d8986585eebf71219f3224
larsenv <larsen@weborican.com>
2017-06-18 13:55:17 -0500

    Finally consolidated all scripts into one

--- Channels/News_Channel/news_anp.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-from newsdownload import download_anp_dutch
-from newsmake import download_source
-
-download_source("ANP Dutch", "anp_dutch", 6, ["065", "066", "067", "074", "076", "077", "078", "079", "082", "083", "088", "094", "095", "096", "098", "105", "107", "108", "110"], download_anp_dutch())
--- Channels/News_Channel/news_news24_mainichi.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-from newsdownload import download_news24_mainichi_japanese
-from newsmake import download_source
-
-download_source("News24/Mainichi Japanese", "news24_mainichi_japanese", 0, ["001"], download_news24_mainichi_japanese())
