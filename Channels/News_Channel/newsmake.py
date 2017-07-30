58104199795ebf14581cbefbf14c5f1d4ca98d49
larsenv <larsen@weborican.com>
2017-07-30 11:23:25 -0500

    Fixed force

--- Channels/News_Channel/newsmake.py
+++ Channels/News_Channel/newsmake.py
@@ -108,13 +108,16 @@ def copy_file(mode, system, country, language_code):
 		for hours in range(0, 24):
 			newsfilename = "news.bin.%s.%s.%s" % (str(datetime.utcnow().hour).zfill(2), mode, system)
 			newsfilename2 = "news.bin.%s" % (str(hours).zfill(2))
-			path = "%s/%s/%s/%s/%s" % (file_path, "v3" if system == "wii_u" else "v2", language_code, country, newsfilename2)
+			path = "%s/%s/%s/%s" % (file_path, "v3" if system == "wii_u" else "v2", language_code, country)
 			mkdir_p(path)
+			path = "%s/%s" % (path, newsfilename2)
 			subprocess.call(["cp", newsfilename, path])
 	else:
 		newsfilename = "news.bin.%s.%s.%s" % (str(datetime.utcnow().hour).zfill(2), mode, system)
 		newsfilename2 = "news.bin.%s" % (str(datetime.utcnow().hour).zfill(2))
-		path = "%s/%s/%s/%s/%s" % (file_path, "v3" if system == "wii_u" else "v2", language_code, country, newsfilename2)
+		path = "%s/%s/%s/%s" % (file_path, "v3" if system == "wii_u" else "v2", language_code, country)
+		mkdir_p(path)
+		path = "%s/%s" % (path, newsfilename2)
 		subprocess.call(["cp", newsfilename, path])
 
 """Run the functions to make the news."""
