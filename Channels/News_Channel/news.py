ce23b5263ab952224bd4e572644d238f7dd74ab1
Larsenv <larsen@weborican.com>
2017-09-30 12:00:11 -0500

    Fixed arguments

--- Channels/News_Channel/news.py
+++ Channels/News_Channel/news.py
@@ -17,7 +17,7 @@ if len(sys.argv) > 1:
     elif sys.argv[1] == "reuters_japanese": download_source("Reuters Japanese", "reuters_japanese", 0, ["001"], download_reuters_japanese())
     else:
         print "Invalid argument. Valid arguments include the following:"
-        print "ap_english, ap_spanish, reuters_english, afp_french, zeit_german, ansa_italian, nu_dutch, news24_mainichi_japanese"
+        print "reuters_america_english, reuters_europe_english, expansion_spanish, efe_europe_spanish, afp_french, zeit_german, ansa_italian, nu_dutch, reuters_japanese"
 else:
     download_source("Reuters America English", "reuters_america_english", 1, ["008", "009", "010", "011", "012", "013", "014", "015", "016", "017", "018", "019", "020", "021", "022", "023", "024", "025", "026", "027", "028", "029", "030", "031", "032", "033", "034", "035", "036", "037", "038", "039", "040", "041", "042", "043", "044", "045", "046", "047", "048", "049", "050", "051", "052"], download_reuters_america_english())
     download_source("Reuters Europe English", "reuters_europe_english", 1, ["065", "066", "067", "074", "076", "077", "078", "079", "082", "083", "088", "094", "095", "096", "098", "105", "107", "108", "110"], download_reuters_europe_english())
