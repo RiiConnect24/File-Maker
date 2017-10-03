5bcf6e1e09e4acd9b935de29b044c4d3c2e1713c
Larsen Vallecillo <larsen@Weborican.com>
2017-10-03 15:26:56 -0500

    removed try except

--- Channels/News_Channel/newsdownload.py
+++ Channels/News_Channel/newsdownload.py
@@ -1177,7 +1177,6 @@ def download_ap(topics_name, topics, language):
 			rss_feed = feedparser.parse(requests.get("http://staging.hosted.ap.org/lineups/%s-rss_2.0.xml?SITE=AP&SECTION=HOME" % rss).text)
 
 			for items in rss_feed.entries:
-				try:
 					updated = parser.parse(items.updated)
 					updated = updated.astimezone(tz.tzutc())
 
@@ -1193,8 +1192,6 @@ def download_ap(topics_name, topics, language):
 						parsedata = parsedata_ap(items["link"], items["title"], updated_utc, updated, format, language)
 
 						if parsedata != None: data[rss_category[0] + str(numbers)] = parsedata
-				except:
-					print "Failed."
 
 		print "\n"
 
