c1721c902b875b2451eeebed71fdbaa51f9238e7
Larsenv <larsen@weborican.com>
2017-10-21 13:44:33 -0500

    Fixed timestamp for forecast

--- Channels/Forecast_Channel/forecast.py
+++ Channels/Forecast_Channel/forecast.py
@@ -134,7 +134,7 @@ def mph_kmh(wind): return int(round(float(wind)*1.60934))
 
 def time_convert(time):
 	if mode == 1: return int((time - 946684800) / 60)
-	elif mode == 2: return int((time - 789507840) / 60) # Still have to adjust this.
+	elif mode == 2: return int((time - 789563880) / 60)
 
 def get_epoch(): return int(time.time())
 
