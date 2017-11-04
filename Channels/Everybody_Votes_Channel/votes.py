203f34a53e811331a686ed205042912fe883b664
Larsenv <larsen@weborican.com>
2017-11-04 13:34:12 -0500

    Fixed mysql close

--- Channels/Everybody_Votes_Channel/votes.py
+++ Channels/Everybody_Votes_Channel/votes.py
@@ -154,7 +154,6 @@ def mysql_connect():
 	  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR: print "Something is wrong with your user name or password"
 	  elif err.errno == errorcode.ER_BAD_DB_ERROR: print "Database does not exist"
 	  else: print err
-	else: cnx.close()
 
 def mysql_get_votes():
 	cursor = cnx.cursor()
