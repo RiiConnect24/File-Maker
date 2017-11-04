e9c883858df614424efd61b4e22540edb40f8586
Larsenv <larsen@weborican.com>
2017-11-04 13:35:09 -0500

    Fixed question ID

--- Channels/Everybody_Votes_Channel/votes.py
+++ Channels/Everybody_Votes_Channel/votes.py
@@ -158,7 +158,7 @@ def mysql_connect():
 def mysql_get_votes():
 	cursor = cnx.cursor()
 	question_id = 955 # Just for now :P
-	query = ("SELECT * from EVC.votes WHERE questionID = " % question_id)
+	query = ("SELECT * from EVC.votes WHERE questionID = 955")
 	cursor.execute(query)
 
 	male_voters_response_1 = 0
