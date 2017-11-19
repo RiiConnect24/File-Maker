48547767f7436b1fcdd2f81c784e4356698f1897
Larsenv <larsen@weborican.com>
2017-11-19 13:34:14 -0600

    Fixed replace

--- Channels/Everybody_Votes_Channel/votes.py
+++ Channels/Everybody_Votes_Channel/votes.py
@@ -273,7 +273,7 @@ def add_question(poll_id,q,r1,r2,f,c):
 		worldwide_q = True
 
 def question_text_replace(text):
-	text = text.replace(u'\xE2\x80\xA6', " . . .").replace("...", " . . .")
+	text = text.replace("\xE2\x80\xA6", " . . .").replace("...", " . . .")
 	return text
 
 dictionaries = []
