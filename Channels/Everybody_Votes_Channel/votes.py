1ef2cdc9b11a93aa9f99bf997a1bf3b058bf0008
Larsenv <larsen@weborican.com>
2017-11-20 12:08:38 -0600

    Added set_charset_collation

--- Channels/Everybody_Votes_Channel/votes.py
+++ Channels/Everybody_Votes_Channel/votes.py
@@ -168,6 +168,7 @@ def mysql_connect():
 									  	  database=mysql_database,
 										  charset='utf8',
 										  use_unicode=True)
+		cnx.set_charset_collation('utf8')
 	except mysql.connector.Error as err:
 		 if err.errno == errorcode.ER_ACCESS_DENIED_ERROR: print "Something is wrong with your user name or password"
 		 elif err.errno == errorcode.ER_BAD_DB_ERROR: print "Database does not exist"
