e61db958d4df0019eb05c2daa1f6ec1ec6458346
Larsenv <larsen@weborican.com>
2017-12-16 09:45:07 -0600

    Fixed Reuters articles

--- Channels/News_Channel/newsdownload.py
+++ Channels/News_Channel/newsdownload.py
@@ -519,7 +519,7 @@ def parsedata_reuters(language, url, title, updated):
 	try: headline = fix_chars(soup.find("h1", {"class": "ArticleHeader_headline_2zdFM"}).get_text()) # Parse the headline.
 	except: return None
 
-	article_text = BeautifulSoup(str(soup.find("div", {"class": "ArticleBody_body_2ECha"})).replace("</p>", "\n\n</p>"), "lxml").get_text()
+	article_text = BeautifulSoup(str(soup.find("div", {"class": "StandardArticleBody_body_1gnLA"})).replace("</p>", "\n\n</p>"), "lxml").get_text()
 
 	try:
 		if language == "en": article = fix_chars(article_text+ "\n\n" + soup.find("p", {"class": "Attribution_content_27_rw"}).contents[0])
