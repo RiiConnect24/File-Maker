6473c18f24b6a141e329977b000cf832921f3fe1
Larsenv <larsen@weborican.com>
2017-12-30 14:47:01 -0600

    Add script to convert wii timestamp to computer

---
+++ Tools/Time/convert.py
@@ -0,0 +1,11 @@
+import datetime
+import sys
+
+def main():
+    wii_to_timestamp = (int(sys.argv[1]) * 60) + 946684800
+    date = datetime.datetime.utcfromtimestamp(wii_to_timestamp).strftime('%Y-%m-%d %H:%M')
+    print "Timestamp: %s" % wii_to_timestamp
+    print "Date: %s" % date
+
+if __name__ == "__main__":
+    main()
