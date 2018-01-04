d660ccc0b7700693e00db95fb6f0655a94041657
Larsenv <larsen@weborican.com>
2018-01-04 16:35:23 -0600

    Fix timestamp tool

--- Tools/Time/convert.py
+++ Tools/Time/convert.py
@@ -3,7 +3,7 @@ import sys
 
 
 def main():
-    wii_to_timestamp = (int(sys.argv[1]) * 60) + 946684800
+    wii_to_timestamp = (int(sys.argv[1], 16) * 60) + 946684800
     date = datetime.datetime.utcfromtimestamp(wii_to_timestamp).strftime('%Y-%m-%d %H:%M')
     print "Timestamp: %s" % wii_to_timestamp
     print "Date: %s" % date
