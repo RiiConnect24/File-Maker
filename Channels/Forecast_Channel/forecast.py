5a4aa3d70d597c0c33229ad393afc27a7ed08596
Larsenv <larsen@weborican.com>
2018-02-11 09:59:29 -0600

    Add Datadog

--- Channels/Forecast_Channel/forecast.py
+++ Channels/Forecast_Channel/forecast.py
@@ -29,6 +29,7 @@ from datetime import datetime, timedelta
 import cachetclient.cachet
 import requests
 import rsa
+from datadog import statsd
 from raven import Client
 from raven.conf import setup_logging
 from raven.handlers.logging import SentryHandler
@@ -309,6 +310,13 @@ def ui():
         sys.stdout.write(out)
         sys.stdout.flush()
         time.sleep(refresh_rate)
+    """Log stuff to Datadog."""
+    statsd.set("forecast.api_requests", apirequests)
+    statsd.gauge("forecast.retry_count", retrycount)
+    statsd.gauge("forecast.elapsed_time", elapsed_time)
+    statsd.set("forecast.bandwidth_usage", bandwidth)
+    statsd.set("forecast.cities", cities)
+    statsd.gauge("forecast.errors", errors)
     print "\n"
 
 
