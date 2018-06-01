d3b5b0b6f50258cc41b85c9255857652bcd3219e
Larsen Vallecillo <larsen@Weborican.com>
2018-06-01 17:13:31 -0500

    Fix bylines again

--- Channels/News_Channel/newsdownload.py
+++ Channels/News_Channel/newsdownload.py
@@ -529,7 +529,7 @@ class Parse(News):
         if self.article[-2:] == "\n\n":
             self.article = self.article[:-2]
 
-        if self.newsdata["bylines"] != "" or None:
+        if self.newsdata["bylines"] != "" or self.newsdata["bylines"] != None:
             self.article += "\n\n" + self.newsdata["bylines"]
 
         if self.article is None:
