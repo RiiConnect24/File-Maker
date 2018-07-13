205779b25aef126d3486f00ee2478cda4dd84978
Larsenv <larsen@weborican.com>
2018-07-13 18:56:06 -0500

    Revert what didn't work

--- Channels/Forecast_Channel/forecast.py
+++ Channels/Forecast_Channel/forecast.py
@@ -212,7 +212,7 @@ def worker():
             item = q.get_nowait()
             get_data(item[0], item[1])
             q.task_done()
-        except Queue.Empty:
+        except:
             pass
 
 
@@ -1327,7 +1327,7 @@ for list in weathercities:
             weather_data[k] = ElementTree.fromstring(v)
             if weather_data[k].find("{http://www.accuweather.com}failure") is not None:
                 weather_data[k] = None
-        except cElementTree.ParseError:
+        except:
             weather_data[k] = None
         if weather_data[k] is not None:
             get_legacy_api(list, k)
