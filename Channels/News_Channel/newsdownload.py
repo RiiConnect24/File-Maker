66cd766bb476cc39ac359bb3c8aedbeca81219de
Larsen Vallecillo <larsen@Weborican.com>
2018-10-25 09:23:47 -0500

    Fix lepoint parsing

--- Channels/News_Channel/newsdownload.py
+++ Channels/News_Channel/newsdownload.py
@@ -88,7 +88,7 @@ sources = {
     },
     "afp_french": {
         "name": "AFP_French",
-        "url": "http://www.lepoint.fr/24h-infos/rss.xml",
+        "url": "https://www.lepoint.fr/24h-infos/rss.xml",
         "lang": "fr",
         "cat": collections.OrderedDict([
             ("monde", "world"),
