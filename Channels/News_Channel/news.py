6e5340950a3a9067962ca828424ce02b5b988e52
Larsenv <larsen@weborican.com>
2019-08-12 13:14:54 -0500

    Add exception for failed news

--- Channels/News_Channel/news.py
+++ Channels/News_Channel/news.py
@@ -21,25 +21,28 @@ def main():
         download("reuters_japanese")
 
 def download(source):
-    if source == "ap_english":
-        process_news("AP English", "ap_english", 1, "America", News("ap_english"))
-    elif source == "ap_spanish":
-        process_news("AP Spanish", "ap_spanish", 4, "International", News("ap_spanish"))
-    elif source == "reuters_europe_english":
-        process_news("Reuters Europe English", "reuters_europe_english", 1, "Europe", News("reuters_europe_english"))
-    elif source == "afp_french":
-        process_news("AFP French", "afp_french", 3, "International", News("afp_french"))
-    elif source == "dtoday_german":
-        process_news("AFP German", "dtoday_german", 2, "Europe", News("dtoday_german"))
-    elif source == "ansa_italian":
-        process_news("ANSA Italian", "ansa_italian", 5, "Europe", News("ansa_italian"))
-    elif source == "nu_dutch":
-        process_news("NU.nl Dutch", "nu_dutch", 6, "Europe", News("nu_dutch"))
-    elif source == "reuters_japanese":
-        process_news("Reuters Japanese", "reuters_japanese", 0, "Japan", News("reuters_japanese"))
-    else:
-        print("Invalid source specified.")
-        exit()
+    try:
+        if source == "ap_english":
+            process_news("AP English", "ap_english", 1, "America", News("ap_english"))
+        elif source == "ap_spanish":
+            process_news("AP Spanish", "ap_spanish", 4, "International", News("ap_spanish"))
+        elif source == "reuters_europe_english":
+            process_news("Reuters Europe English", "reuters_europe_english", 1, "Europe", News("reuters_europe_english"))
+        elif source == "afp_french":
+            process_news("AFP French", "afp_french", 3, "International", News("afp_french"))
+        elif source == "dtoday_german":
+            process_news("AFP German", "dtoday_german", 2, "Europe", News("dtoday_german"))
+        elif source == "ansa_italian":
+            process_news("ANSA Italian", "ansa_italian", 5, "Europe", News("ansa_italian"))
+        elif source == "nu_dutch":
+            process_news("NU.nl Dutch", "nu_dutch", 6, "Europe", News("nu_dutch"))
+        elif source == "reuters_japanese":
+            process_news("Reuters Japanese", "reuters_japanese", 0, "Japan", News("reuters_japanese"))
+        else:
+            print("Invalid source specified.")
+            exit()
+    except:
+        return
 
 if __name__ == "__main__":
     main()
