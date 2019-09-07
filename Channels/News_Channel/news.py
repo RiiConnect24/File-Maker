f11af65b71c4ea30905a711a1d4dd51cf3269dd7
Larsen Vallecillo <larsen@Weborican.com>
2019-09-07 11:06:36 -0500

    Add Sentry exception capturing

--- Channels/News_Channel/news.py
+++ Channels/News_Channel/news.py
@@ -42,6 +42,7 @@ def download(source):
             print("Invalid source specified.")
             exit()
     except:
+        client.captureException()
         return
 
 if __name__ == "__main__":
