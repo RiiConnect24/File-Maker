5da63eb9d877b3b2c049e816627d9e500d3deec7
larsenv <larsen@weborican.com>
2022-09-09 22:08:45 -0500

    Revert threading again

--- Channels/News_Channel/news.py
+++ Channels/News_Channel/news.py
@@ -19,8 +19,6 @@ def main():
         sources = [
             "ap_english",
             "ap_spanish",
-            "ap_canada",
-            "ap_australia",
             "reuters_europe_english",
             "afp_french",
             "afp_german",
