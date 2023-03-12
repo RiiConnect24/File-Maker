df804f68d41563f93849bfda7bde72a782a19014
Larsen Vallecillo <larsen@Weborican.com>
2023-03-12 16:46:37 -0500

    Fix difflib

--- Channels/News_Channel/newsmake.py
+++ Channels/News_Channel/newsmake.py
@@ -743,7 +743,7 @@ class NewsMake:
                 if topics in keys:
                     numbers += 1
 
-                    newstime[self.data[keys][3] + " - " + self.data[keys][2]] = self.get_timestamp(1) + u32(numbers)
+                    newstime[self.data[keys][3] + b" - " + self.data[keys][2]] = self.get_timestamp(1) + u32(numbers)
 
             pickle.dump(
                 newstime,
